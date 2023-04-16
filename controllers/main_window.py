083038d8408a3aa47029ca1ef53627ff97c30ab5
Xukay101 <zaca03zaca@gmail.com>
2023-04-15 23:55:07 -0300

    Update template

--- controllers/main_window.py
+++ controllers/main_window.py
@@ -1,13 +1,13 @@
 from PySide6.QtWidgets import QWidget
 
-from views.ui_main_window import MainWindow
+from views.main_window_ui import Ui_MainWindow
 
-class MainWindowForm(QWidget, MainWindow):
+class MainWindowForm(QWidget, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
 
-        self.num1_button.clicked.connect(self.hello)
+        self.test_button.clicked.connect(self.hello)
 
     def hello(self):
         print('Hello World')
---
+++ views/main_window_ui.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'main_window.ui'
+##
+## Created by: Qt User Interface Compiler version 6.5.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QPushButton, QSizePolicy, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(450, 266)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.centralwidget.setGeometry(QRect(0, 0, 431, 258))
+        self.test_button = QPushButton(self.centralwidget)
+        self.test_button.setObjectName(u"test_button")
+        self.test_button.setGeometry(QRect(180, 110, 93, 28))
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Codificador/Decodificador", None))
+        self.test_button.setText(QCoreApplication.translate("MainWindow", u"Test", None))
+    # retranslateUi
+
